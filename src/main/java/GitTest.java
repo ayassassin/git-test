80fec420a3cc1af8de35ca194e6a63765be40696
ayassassin <118879276+ayassassin@users.noreply.github.com>
2022-11-23 17:54:44 +0800

    pull test

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hot-fix01");
         System.out.println("push commit");
+        System.out.println("pull test")
     }
 }
