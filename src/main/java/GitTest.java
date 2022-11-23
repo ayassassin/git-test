ef6334de67907329cdc7460b679a80e3c27f7597
wspasy <2862252549@qq.com>
2022-11-23 16:27:52 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author assassin
+ * @version 1.0
+ * Create by 2022/11/23 16:25
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
